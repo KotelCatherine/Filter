79d22028d3300d9771f6c982e970ded429bd83fe
cotelCatherine <bezmozggoff@gmail.com>
2026-02-18 19:38:24 +0700

    подкорректировала класс ArrayFilter.java

--- src/main/java/ru/itk/ArrayFilter.java
+++ src/main/java/ru/itk/ArrayFilter.java
@@ -11,35 +11,50 @@ public class ArrayFilter {
         try {
 
             Filter<T> castFilter = filterClass.getDeclaredConstructor().newInstance();
-            int count = 0;
-
-            for (T element : array) {
-                if (castFilter.apply(element) != null) {
-                    count++;
-                }
-            }
+            int count = countNotNullElement(array, castFilter);
 
             T element = array[0];
             T[] newArray = (T[]) Array.newInstance(element.getClass(), count);
-            int newArrayIndex = 0;
 
-            for (T t : array) {
-
-                T filtredValue = castFilter.apply(t);
-
-                if (filtredValue != null) {
-                    newArray[newArrayIndex++] = filtredValue;
-                }
-
-            }
-
-            return newArray;
+            return fillNewArrayFiltredElemet(array, castFilter, newArray);
 
         } catch (NoSuchMethodException ex) {
             throw new RuntimeException("Подходящий конструктор не найден!");
         } catch (Exception ex) {
             throw new RuntimeException("Не удалось создать фильтр");
         }
+
+    }
+
+    private <T> T[] fillNewArrayFiltredElemet(T[] array, Filter<T> castFilter, T[] newArray) {
+
+        int newArrayIndex = 0;
+
+        for (T t : array) {
+
+            T filtredValue = castFilter.apply(t);
+
+            if (filtredValue != null) {
+                newArray[newArrayIndex++] = filtredValue;
+            }
+
+        }
+
+        return newArray;
+
+    }
+
+    private <T> int countNotNullElement(T[] array, Filter<T> castFilter) {
+
+        int count = 0;
+
+        for (T element : array) {
+            if (castFilter.apply(element) != null) {
+                count++;
+            }
+        }
+
+        return count;
 
     }
 
